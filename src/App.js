45cbebadbe98de05e7dab053ab027cfe17e385e0
{psw9058) <s05150732w@gmail.com>
2022-01-01 22:35:58 +0900

    delete css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      <header>
+        테스트
       </header>
     </div>
   );
