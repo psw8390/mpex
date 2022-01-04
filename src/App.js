b4f86fc0209f6577953042d4ada01bd7b5b76bd0
{psw9058) <s05150732w@gmail.com>
2022-01-04 18:50:09 +0900

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div >
       <header>
-        테스트
+        <Navbar />
       </header>
     </div>
   );
