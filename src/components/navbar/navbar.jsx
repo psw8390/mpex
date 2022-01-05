0f01cd8042785ce0237746ce8797a95fd54df433
{psw9058) <s05150732w@gmail.com>
2022-01-05 23:06:14 +0900

    making navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from "./navbar.module.css";
+import homeImg from "./home.png";
 
-function Box() {
-  return <div className={styles.Box}>{styles.Box}</div>;
+function Navbar() {
+  return (
+  <div className={styles.Navbar}>
+    <div className={styles.container}></div>
+      <div className={styles.navbar_contents}>
+        <img className={styles.img} alt="home" src={homeImg} />
+        <div className={styles.contentBox}>공지사항</div>
+        <div className={styles.contentBox}>공지사항</div>
+        <div className={styles.contentBox}>공지사항</div>
+        <div className={styles.contentBox}>공지사항</div>
+        <div className={styles.contentBox}>공지사항</div>
+      </div>
+    <div className={styles.container}></div>
+  </div>
+  )
 }
 
-export default Box;
+export default Navbar;
