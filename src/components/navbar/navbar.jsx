9194448912b7a48a5dbe8205a767942898f50553
{psw9058) <s05150732w@gmail.com>
2022-01-06 16:50:40 +0900

    making searchbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,11 +8,13 @@ function Navbar() {
     <div className={styles.container}></div>
       <div className={styles.navbar_contents}>
         <img className={styles.img} alt="home" src={homeImg} />
+        <div className={styles.contentBox}>매칭신청</div>
         <div className={styles.contentBox}>공지사항</div>
-        <div className={styles.contentBox}>공지사항</div>
-        <div className={styles.contentBox}>공지사항</div>
-        <div className={styles.contentBox}>공지사항</div>
-        <div className={styles.contentBox}>공지사항</div>
+        <div className={styles.contentBox}>커뮤니티</div>
+        <input type="text" className={styles.searchBar} placeholder="" />
+        <span className={styles.search__icon}>
+          <i class="far fa-search"></i>
+        </span>
       </div>
     <div className={styles.container}></div>
   </div>
