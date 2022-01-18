43b1da4e95eb80de7ec267a9f3ca3918c85b4447
{psw9058) <s05150732w@gmail.com>
2022-01-18 18:09:34 +0900

    pagination controller update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import HomeBanner from './components/homeBanner/homeBanner';
 import Navbar from './components/navbar/navbar';
+import Searchbar from './components/searchBar/searchbar';
 
 function App() {
   return (
     <div >
-      <header>
-        <Navbar />
-        <HomeBanner />
-      </header>
+      <Navbar />
+      <HomeBanner />
+      <Searchbar />
     </div>
   );
 }
---
+++ src/components/searchBar/searchbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./searchbar.module.css";
+
+function Searchbar() {
+  return (
+    <div className={styles.searchBox}>
+      10000
+    </div>
+  )
+}
+
+export default Searchbar;
