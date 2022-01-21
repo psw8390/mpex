6cddab18cac954d4a3fc952459334cf486be0244
{psw9058) <s05150732w@gmail.com>
2022-01-21 21:44:59 +0900

    box overlap error

--- src/components/searchBar/searchbar.jsx
+++ src/components/searchBar/searchbar.jsx
@@ -4,7 +4,9 @@ import styles from "./searchbar.module.css";
 function Searchbar() {
   return (
     <div className={styles.searchBox}>
-      <div className={styles.searchBoxTitle}>MPEX 에서 가치를 높이세요</div>
+      <div className={styles.searchBoxTitle}>
+        MPEX 에서 가치를 높이세요
+      </div>
     </div>
   )
 }
