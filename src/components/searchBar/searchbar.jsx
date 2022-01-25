091e01540146d729649149e239b582a96648abcf
{psw9058) <s05150732w@gmail.com>
2022-01-25 19:43:12 +0900

    make tagsectioncontainer

--- src/components/searchBar/searchbar.jsx
+++ src/components/searchBar/searchbar.jsx
@@ -11,12 +11,29 @@ function Searchbar() {
       </h1>
       <div class="search_wrapper">
         <input className={styles.searchInput} type="text" placeholder="함께하고 싶은 운동을 입력해보세요." />
-          <button className={styles.buttonSubmit}>
-            <span>
-              <FontAwesomeIcon icon={faSearch} />
-            </span>
-          </button>
+        <button className={styles.buttonSubmit}>
+          <span>
+            <FontAwesomeIcon icon={faSearch} />
+          </span>
+        </button>
+        <div className={styles.tagSection}>
+          <ul className={styles.tagSectionContainer}>
+            <li className={styles.tagSectionItem}>
+              <a href="https://naver.com">#농구</a>
+            </li>
+          </ul>
+          <ul className={styles.tagSectionContainer}>
+            <li className={styles.tagSectionItem}>
+              <a href="https://naver.com">#축구</a>
+            </li>
+          </ul>
+          <ul className={styles.tagSectionContainer}>
+            <li className={styles.tagSectionItem}>
+              <a href="https://naver.com">#야구</a>
+            </li>
+          </ul>
         </div>
+      </div>
     </div>
   )
 }
