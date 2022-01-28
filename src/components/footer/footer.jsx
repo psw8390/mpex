a3037f5ce4d27665506f4d584e68a307a6b309a7
{psw9058) <s05150732w@gmail.com>
2022-01-28 16:54:51 +0900

    making footer box

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "./footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-solid-svg-icons";
+import { faBootstrap, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
     <div className={styles.footerBox}>
-      <div className={styles.divider}></div>
-      10000
+        <div>Contact Me!</div>
+        <div>psw8390@gmail.com</div>
+        <div>
+        <FontAwesomeIcon icon={faGithub} className="faGithub" />
+        <FontAwesomeIcon icon={faBootstrap} className="faBootstrab" />
+        </div>
+        <div>2022 Psu - All rights reserved</div>
     </div>
   )
 }
