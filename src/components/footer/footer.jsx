7958c6a861dcac531654b8ff646b00f20e3b5aff
{psw9058) <s05150732w@gmail.com>
2022-01-29 23:14:44 +0900

    footer contents update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import styles from "./footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faBootstrap, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
     <div className={styles.footerBox}>
-        <div>Contact Me!</div>
-        <div>psw8390@gmail.com</div>
-        <div>
-        <FontAwesomeIcon icon={faGithub} className="faGithub" />
-        <FontAwesomeIcon icon={faBootstrap} className="faBootstrab" />
-        </div>
         <div>2022 Psu - All rights reserved</div>
+        <div>
+          <FontAwesomeIcon icon={faEnvelope} className="faEnvelope" />
+          psw8390@gmail.com
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faGithub} className="faGithub" />
+          https://github.com/psw9058
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faBootstrap} className="faBootstrab" />
+          https://blog.naver.com/psw9058
+        </div>
     </div>
   )
 }
