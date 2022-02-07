4a30c88e414905c8d928f23b9e78c746d2a52ca4
{psw9058) <s05150732w@gmail.com>
2022-02-07 19:09:35 +0900

    contentsBox write content

--- src/components/searchBar/searchbar.jsx
+++ src/components/searchBar/searchbar.jsx
@@ -50,8 +50,13 @@ function Searchbar() {
       <div className={styles.matchigBox}>
         <img src={contentImg} className={styles.contentImage} alt=''/>
         <div className={styles.matchingInfo}>
-          <div>목포대학교</div>
+          <div>목포대학교 (전라남도 목포시 부흥로 10019-10)</div>
+          <div></div>
+          <div>담당자 연락처 : 010-0000-0000</div>
           <div>남여 모두 5:5일반</div>
+          <div>운동시간: 17:00 ~ 19:00</div>
+          <div>게스트비용: 5000원</div>
+          <div>준비물: 흰색/검정색 유니폼</div>
         </div>
         <div className={styles.mattchingUI}>
           <div>매칭신청</div>
