2ccc84741e90f31a53a25d700dca9aebb974b517
{psw9058) <s05150732w@gmail.com>
2022-02-09 18:24:16 +0900

    navbar a upadate

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import homeImg from "./home.jpeg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   return (
   <div className={styles.Navbar}>
     <div className={styles.container}></div>
       <div className={styles.navbar_contents}>
-        <img className={styles.img} alt="home" src={homeImg} />
-        <div className={styles.contentBox}>매칭신청</div>
-        <div className={styles.contentBox}>매칭등록</div>
-        <div className={styles.contentBox}>공지사항</div>
-        <div className={styles.contentBox}>커뮤니티</div>
-        <div className={styles.searchBarBox}>
-          <input type="text" className={styles.searchBar} placeholder="" />
-          <span className={styles.search__icon} placeholder="">
-            <FontAwesomeIcon icon={faSearch} />
-          </span>
+        <a href="/">
+          <img className={styles.img} alt="home" src={homeImg} />
+        </a>
+        <div className={styles.contentBox}>
+          <a href="/apply">참가신청</a>
+        </div>
+        <div className={styles.contentBox}>
+          <a href="/register">매칭등록</a>
+        </div>
+        <div className={styles.contentBox}>
+          <a href="/notice">공지사항</a>
+        </div>
+        <div className={styles.contentBox}>
+          <a href="/community">커뮤니티</a>
         </div>
         <div className={styles.loginBox}>
           <button className={styles.loginbtn}>로그인</button>
