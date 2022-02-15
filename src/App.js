609f76d66e9d7226a350938746997f66ad1750ff
{psw9058) <s05150732w@gmail.com>
2022-02-15 21:52:43 +0900

    react router error

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import HomeBanner from './components/homeBanner/homeBanner';
 import Navbar from './components/navbar/navbar';
 import Matchingbox from './components/matchingBox/matchingbox';
 import Footer from './components/footer/footer';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Home from './components/home';
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div >
       <Navbar />
       <HomeBanner />
       <Matchingbox />
       <Footer />
     </div>
+
+      <Routes>
+        <Route exact path='/home' component={Home} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,36 +1,35 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import homeImg from "./home.jpeg";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-<BrowserRouter>
   <div className={styles.Navbar}>
     <div className={styles.container}></div>
       <div className={styles.navbar_contents}>
-        <Link to="/">
+        <Link to='/home'>
           <img className={styles.img} alt="home" src={homeImg} />
         </Link>
         <div className={styles.contentBox}>
-          <Link to="/home">
+         
             참가신청
-          </Link>
+         
         </div>
         <div className={styles.contentBox}>
-          <Link to="/register">
+        
             매칭등록
-          </Link>
+       
         </div>
         <div className={styles.contentBox}>
-          <Link to="/notice">
+          
             공지사항
-          </Link>
+      
         </div>
         <div className={styles.contentBox}>
-          <Link to="community">
+         
             커뮤니티
-          </Link>
+        
         </div>
         <div className={styles.loginBox}>
           <button className={styles.loginbtn}>
@@ -40,7 +39,6 @@ function Navbar() {
       </div>
     <div className={styles.container}></div>
   </div>
-</BrowserRouter>
   )
 }
 
