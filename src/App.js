31acbcf040d5ee28a5ffbb781b9b4ddd6a8fbb2b
{psw9058) <s05150732w@gmail.com>
2022-02-16 20:37:32 +0900

    router error resolce

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import './App.css';
-import HomeBanner from './components/homeBanner/homeBanner';
-import Navbar from './components/navbar/navbar';
-import Matchingbox from './components/matchingBox/matchingbox';
-import Footer from './components/footer/footer';
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from './components/home';
 
@@ -11,15 +7,9 @@ import Home from './components/home';
 function App() {
   return (
     <BrowserRouter>
-    <div >
-      <Navbar />
-      <HomeBanner />
-      <Matchingbox />
-      <Footer />
-    </div>
-
       <Routes>
-        <Route exact path='/home' component={Home} />
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/home' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Navbar from './navbar/navbar';
+import HomeBanner from './homeBanner/homeBanner';
+import Matchingbox from './matchingBox/matchingbox';
+import Footer from './footer/footer';
 
 class Home extends React.Component {
   render() {
     return(
-      <h1>This is Home Component</h1>
+      <div >
+        <Navbar />
+        <HomeBanner />
+        <Matchingbox />
+        <Footer />
+    </div>
     )
   }
 }
