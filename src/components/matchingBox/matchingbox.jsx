52145a99371881a9a16dd9e043941543248aff4f
{psw9058) <s05150732w@gmail.com>
2022-02-26 00:32:46 +0900

    footer box bottom fixed error

--- src/components/matchingBox/matchingbox.jsx
+++ src/components/matchingBox/matchingbox.jsx
@@ -74,6 +74,25 @@ function Matchingbox() {
           <div>03/15명</div>
         </div>
       </div>
+      <div className={styles.matchigBox}>
+        <div className={styles.matchigBoxTime}>
+          <div>11:00</div>
+          <div>~</div>
+          <div>15:00</div>
+        </div>
+        <img className={styles.gymImg} alt="homeBaneer" src={gymImg} />
+        <div className={styles.matchingInfo}>
+          <div>목포대학교 부설 체육관</div>
+          <div>농구 5:5 풀코트</div>
+          <div className={styles.significant}>특이사항 : </div>
+        </div>
+        <div className={styles.mattchingUI}>
+          <button>
+            <a href="https://naver.com">매칭신청</a>
+          </button>
+          <div>03/15명</div>
+        </div>
+      </div>
     </div>
   )
 }
