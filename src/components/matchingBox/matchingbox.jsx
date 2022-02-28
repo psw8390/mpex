e4f2311786f049914bb66e2915a05df49ddd8324
{psw9058) <s05150732w@gmail.com>
2022-02-28 15:44:56 +0900

    gym info input box create

--- src/components/matchingBox/matchingbox.jsx
+++ src/components/matchingBox/matchingbox.jsx
@@ -55,6 +55,26 @@ function Matchingbox() {
           </ul>
         </div>
       </div>
+      <form className={styles.matchigBox}>
+        <div className={styles.matchigBoxTime}>
+          <input placeholder='운동시작시간'></input>
+          <div>~</div>
+          <input placeholder='운동종료시간'></input>
+        </div>
+        <input type="file" className={styles.gymImg} />
+        <div className={styles.matchingInfo}>
+          <input placeholder='장소명'></input>
+          <input placeholder='경기방식'></input>
+          <input placeholder='특이사항:'></input>
+        </div>
+        <div className={styles.mattchingUI}>
+          <button>
+            <a href="https://naver.com">매칭신청</a>
+          </button>
+          <div>03/15명</div>
+        </div>
+      </form>
+
       <div className={styles.matchigBox}>
         <div className={styles.matchigBoxTime}>
           <div>11:00</div>
