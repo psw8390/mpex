1839ae07d91de288d56ada13a922e8784c381019
{psw9058) <s05150732w@gmail.com>
2022-03-05 17:26:39 +0900

    matching register css update

--- src/components/matchingRegister/matchingRegister.jsx
+++ src/components/matchingRegister/matchingRegister.jsx
@@ -10,21 +10,21 @@ function MatchingRegister() {
           <input placeholder='운동시작시간 ex)23:00'></input>
         </div>
 
-        <div className="filebox"> 
-          <label for="ex_file" className='fileboxlabel'>업로드</label> 
-          <input type="file" id="ex_file"></input>
+        <div className={styles.filebox}> 
+          <label for="ex_file" className='fileboxlabel'>체육관 이미지 업로드</label> 
+          <input type="file"></input>
         </div>
 
         <div className={styles.matchingInfo}>
-          <input placeholder='장소명'></input>
-          <input placeholder='경기방식'></input>
+          <input placeholder='장소명:'></input>
+          <input placeholder='경기방식:'></input>
           <input placeholder='특이사항:'></input>
         </div>
         <div className={styles.mattchingUI}>
           <button>
             <a href="https://naver.com">매칭등록</a>
           </button>
-          <input placeholder='최대인원'></input>
+          <input placeholder='최대인원:'></input>
         </div>
       </form>
   )
