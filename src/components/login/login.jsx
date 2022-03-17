ef6afead6502f65fe80c4a8def14290e925f22a3
{psw9058) <s05150732w@gmail.com>
2022-03-17 16:56:18 +0900

    env error solved

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import LoginFoot from '../loginFoot/loginFoot';
 import LoginHead from '../loginHead/loginFoot';
 
+
 const Login = ({authService}) => {
+  const onLogin = event => {
+    authService
+      .login(event.currentTarget.textContent)
+      .then(console.log('1'));
+  };
   return (
     <section>
       <LoginHead />
@@ -10,10 +16,10 @@ const Login = ({authService}) => {
         <h1>로그인</h1>
         <ul>
           <li>
-            <button>Google</button>
+            <button onClick={onLogin}>Google</button>
           </li>
           <li>
-            <button>Github</button>
+            <button onClick={onLogin}>Github</button>
           </li>
         </ul>
       </section>
