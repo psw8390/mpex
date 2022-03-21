bfa5404141a5883d6445bb6aafc82fa6462fdec6
{psw9058) <s05150732w@gmail.com>
2022-03-21 15:53:43 +0900

    login authservice error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import Home from './components/home';
 import Notice from './components/notice';
 import Community from './components/community';
+import Login from './components/login/login';
 
-function App() {
+function App({authService}) {
   return (
+    <div className={styles.app}>
     <BrowserRouter>
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/' element={<Login authService={authService} />} />
+        <Route exact path='/home' element={<Home authService={authService} />} />
         <Route exact path='/notice' element={<Notice />} />
         <Route exact path='/community' element={<Community />} />
       </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
