2a72930da294ac85dc1f48bff99be1395ce401b5
{psw9058) <s05150732w@gmail.com>
2022-03-22 12:17:48 +0900

    logo.png error

--- src/components/loginHead/loginHead.jsx
+++ src/components/loginHead/loginHead.jsx
@@ -3,7 +3,7 @@ import styles from './loginHead.module.css';
 
 const LoginHead = (props) => (
     <header className={styles.header}>
-      <img className={styles.logo} src="/images/logo.png" alt="logo" />
+      <img className={styles.logo} src={process.env.PUBLIC_URL + '/images/logo.png'} alt="logo" />
       <h1 className={styles.title}>MPEX - Exercise Matching service</h1>
     </header>
   );
